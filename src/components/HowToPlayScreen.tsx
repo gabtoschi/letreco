2570d52e238e90460e9fa2784db9c45f8df1bb53
Gabriel Toschi <gabriel.toschi@hotmail.com>
2022-01-06 19:47:37 -0300

    New info on how to play

--- src/components/HowToPlayScreen.tsx
+++ src/components/HowToPlayScreen.tsx
@@ -20,31 +20,43 @@ function HowToPlayScreen(props: HowToPlayScreenProps) {
         <hr/>
 
         <p className="text-center">Se uma letra ficar verde, ela está na palavra e na posição correta.</p>
-        <div className="d-flex align-items-center justify-content-around mb-4">
-          <GuessLetterView letter="V" state="right"></GuessLetterView>
-          <GuessLetterView letter="E" state="disabled"></GuessLetterView>
-          <GuessLetterView letter="R" state="disabled"></GuessLetterView>
-          <GuessLetterView letter="D" state="disabled"></GuessLetterView>
-          <GuessLetterView letter="E" state="disabled"></GuessLetterView>
+        <div className="d-flex justify-content-center align-items-center mb-4">
+          <div className="d-flex">
+            <GuessLetterView letter="V" state="right" marginRight={true}></GuessLetterView>
+            <GuessLetterView letter="E" state="disabled" marginRight={true}></GuessLetterView>
+            <GuessLetterView letter="R" state="disabled" marginRight={true}></GuessLetterView>
+            <GuessLetterView letter="D" state="disabled" marginRight={true}></GuessLetterView>
+            <GuessLetterView letter="E" state="disabled"></GuessLetterView>
+          </div>
         </div>
 
         <p className="text-center">Se uma letra ficar amarela, ela está na palavra, mas na posição errada.</p>
-        <div className="d-flex align-items-center justify-content-around mb-4">
-          <GuessLetterView letter="A" state="disabled"></GuessLetterView>
-          <GuessLetterView letter="U" state="disabled"></GuessLetterView>
-          <GuessLetterView letter="R" state="displaced"></GuessLetterView>
-          <GuessLetterView letter="E" state="disabled"></GuessLetterView>
-          <GuessLetterView letter="O" state="disabled"></GuessLetterView>
+        <div className="d-flex justify-content-center align-items-center mb-4">
+          <div className="d-flex">
+            <GuessLetterView letter="A" state="disabled" marginRight={true}></GuessLetterView>
+            <GuessLetterView letter="U" state="disabled" marginRight={true}></GuessLetterView>
+            <GuessLetterView letter="R" state="displaced" marginRight={true}></GuessLetterView>
+            <GuessLetterView letter="E" state="disabled" marginRight={true}></GuessLetterView>
+            <GuessLetterView letter="O" state="disabled"></GuessLetterView>
+          </div>
         </div>
 
         <p className="text-center">Se uma letra ficar vermelha, ela NÃO está na palavra.</p>
-        <div className="d-flex align-items-center justify-content-around mb-4">
-          <GuessLetterView letter="R" state="disabled"></GuessLetterView>
-          <GuessLetterView letter="U" state="disabled"></GuessLetterView>
-          <GuessLetterView letter="B" state="disabled"></GuessLetterView>
-          <GuessLetterView letter="R" state="disabled"></GuessLetterView>
-          <GuessLetterView letter="O" state="wrong"></GuessLetterView>
+        <div className="d-flex justify-content-center align-items-center mb-4">
+          <div className="d-flex">
+            <GuessLetterView letter="R" state="disabled" marginRight={true}></GuessLetterView>
+            <GuessLetterView letter="U" state="disabled" marginRight={true}></GuessLetterView>
+            <GuessLetterView letter="B" state="disabled" marginRight={true}></GuessLetterView>
+            <GuessLetterView letter="R" state="disabled" marginRight={true}></GuessLetterView>
+            <GuessLetterView letter="O" state="wrong"></GuessLetterView>
+          </div>
         </div>
+
+        <p className="text-center credits">
+          criado por <a href="https://gabtoschi.com" target="_blank">Gabriel Toschi</a><br/>
+          versão não-oficial do <a href="https://www.powerlanguage.co.uk/wordle/" target="_blank">Wordle</a><br/>
+          powered by <a href="https://pt-br.reactjs.org/" target="_blank">React</a>, <a href="https://getbootstrap.com/" target="_blank">Bootstrap</a>, <a href="https://pages.github.com/" target="_blank">GitHub Pages</a><br/>
+        </p>
 
         <div className="d-flex align-items-center justify-content-center">
           <button
