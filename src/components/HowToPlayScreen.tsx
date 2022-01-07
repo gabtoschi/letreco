8d927e5c37dd10b9f89035e409867e05a4d1ac12
Gabriel Toschi <gabriel.toschi@hotmail.com>
2022-01-07 10:03:47 -0300

    Change how to play text

--- src/components/HowToPlayScreen.tsx
+++ src/components/HowToPlayScreen.tsx
@@ -14,13 +14,12 @@ function HowToPlayScreen(props: HowToPlayScreenProps) {
           Todos os dias, uma nova palavra aparecerá no Letreco para você adivinhar.<br/>
           Você terá 6 tentativas. Cada uma delas deve ser uma palavra que exista.<br/>
           Acentos e cedilha são ignorados, tanto nas tentativas, quanto na resposta.<br/>
-          Após enviar uma tentativa, a cor das letras mudará para indicar o quão perto você está da resposta.<br/>
-          Ao fim do jogo, você pode compartilhar/copiar o seu resultado.
+          Após enviar uma tentativa, a cor das letras mudará para indicar o quão perto você está da resposta.
         </p>
 
         <hr/>
 
-        <p className="text-center">Se uma letra ficar verde, ela está na palavra e na posição correta.</p>
+        <p className="text-center">Se uma letra ficar verde, ela está presente na palavra e na posição correta.</p>
         <div className="d-flex justify-content-center align-items-center mb-4">
           <div className="d-flex">
             <GuessLetterView letter="V" state="right" marginRight={true}></GuessLetterView>
@@ -31,7 +30,7 @@ function HowToPlayScreen(props: HowToPlayScreenProps) {
           </div>
         </div>
 
-        <p className="text-center">Se uma letra ficar amarela, ela está na palavra, mas na posição errada.</p>
+        <p className="text-center">Se uma letra ficar amarela, ela está presente na palavra, mas na posição errada.</p>
         <div className="d-flex justify-content-center align-items-center mb-4">
           <div className="d-flex">
             <GuessLetterView letter="A" state="disabled" marginRight={true}></GuessLetterView>
