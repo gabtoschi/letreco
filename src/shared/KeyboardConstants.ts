815a7bd51a4f5b47cd22a4ab414898354cf1164f
Juliano Braz <jbraz@bionexo.com>
2022-09-02 09:12:53 -0300

    refactor(constant): create a shared folder to save constants

---
+++ src/shared/KeyboardConstants.ts
@@ -0,0 +1,5 @@
+export const KEY_LEFT = 'ArrowLeft';
+export const KEY_RIGHT = 'ArrowRight';
+export const KEY_BACKSPACE = 'Backspace';
+export const KEY_ENTER = 'Enter';
+export const KEY_LETTERS = 'abcdefghijklmnopqrstuvwxyz';
