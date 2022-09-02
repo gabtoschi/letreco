2b83127e0edca810adc2d739c38d8bbe6298f06f
Juliano Braz <jbraz@bionexo.com>
2022-09-02 09:12:11 -0300

    improve(guessLetter): add underline style and behaviour

--- src/components/GuessLetterView.tsx
+++ src/components/GuessLetterView.tsx
@@ -1,19 +1,44 @@
 import { useContext } from "react";
 import { GlobalSettingsContext } from "../hooks/useGlobalSettings";
+import { useUnderlinePosition } from "../hooks/useUnderlinePosition";
 import { GuessLetterViewProps } from "../models";
 import '../styles/GuessLetterView.css';
 
 function GuessLetterView(props: GuessLetterViewProps) {
+  const { underlinePosition, setSpecificUnderlinePosition } = useUnderlinePosition();
   const [{isColorblindModeActive}] = useContext(GlobalSettingsContext);
 
-  const className = "letter-wrapper rounded d-flex justify-content-center align-items-center "
-    + props.state
+  const getAlign = () : string => {
+    return props.letter ? "justify-content-center" : "justify-content-end";
+  }
+
+  const isValidaState = () : boolean => {
+    const validStates = [undefined, "typing", "wordlistError"];
+    return validStates.includes(props.state);
+  }
+
+  const canAddUnderscore = () : boolean => {
+    if (underlinePosition !== props.index) return false;
+
+    return isValidaState();
+  }
+
+  const onClick = () : void => {
+    if(props.index === undefined) return;
+    if(!isValidaState()) return;
+
+    setSpecificUnderlinePosition(props.index);
+  }
+
+  const className = `letter-wrapper rounded d-flex ${getAlign()} align-items-center `
+    + `${props.state && props.state}`
     + ( !!props.marginRight ? ' me-2' : '' )
     + ( isColorblindModeActive ? ' colorblind': '' );
 
   return (
-    <div className={className}>
+    <div className={className} onClick={onClick}>
       {props.letter}
+      {canAddUnderscore() && <div className="letter-underline-active"></div>}
     </div>
   );
 }
