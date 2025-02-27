f8b898f87ba41a169b3877afd3beecf14bcb6de7
Gabriel Toschi <gabriel.toschi@hotmail.com>
2025-02-26 23:46:20 -0300

    Adiciona banner Piquinim

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,10 @@
 import Game from "./Game";
 import Header from "./Header";
-import '../styles/App.css';
-import { GlobalSettingsContext, useGlobalSettings } from "../hooks/useGlobalSettings";
+import "../styles/App.css";
+import {
+  GlobalSettingsContext,
+  useGlobalSettings,
+} from "../hooks/useGlobalSettings";
 import { StatisticsContext, useStatistics } from "../hooks/useStatistics";
 
 function App() {
@@ -10,14 +13,25 @@ function App() {
 
   return (
     <StatisticsContext.Provider value={statistics}>
-    <GlobalSettingsContext.Provider value={globalSettings}>
+      <GlobalSettingsContext.Provider value={globalSettings}>
+        <div className="app-container">
+          <Header />
+          <Game />
 
-      <div className="app-container">
-        <Header />
-        <Game />
-      </div>
-
-    </GlobalSettingsContext.Provider>
+          <a
+            className="mt-5 w-100 d-flex justify-content-center align-items-center"
+            href="https://meeplestarter.com.br/piquinim"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              className="w-100 mb-2"
+              src="letreco/piquinim.png"
+              alt="Conheça a Coleção Piquinim, jogos de tabuleiro que cabem no bolso!"
+            />
+          </a>
+        </div>
+      </GlobalSettingsContext.Provider>
     </StatisticsContext.Provider>
   );
 }
